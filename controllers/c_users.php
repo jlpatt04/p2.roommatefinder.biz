a9c63163e3ea4f2ddfe7977b0d98fae67f7bef82
Jen <jen.patten@gmail.com>
2013-11-06 07:41:43 -0500

    minor image changes

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -79,9 +79,11 @@ class users_controller extends base_controller {
         #Pass data to view
         $this->template->content->error = $error;
         
+        #CSS file for Login
         $client_files_head = Array('/css/login.css');
-    	
-    	$this->template->client_files_head = Utils::load_client_files($client_files_head);
+
+    	#Pass the date to the view
+        $this->template->client_files_head = Utils::load_client_files($client_files_head);
         
         #Render template
         echo $this->template;
@@ -91,29 +93,29 @@ class users_controller extends base_controller {
     }
     
     public function p_login() {
-    # Sanitize the user entered data to prevent any funny-business (re: SQL Injection Attacks)
-    $_POST = DB::instance(DB_NAME)->sanitize($_POST);
+        # Sanitize the user entered data to prevent any funny-business (re: SQL Injection Attacks)
+        $_POST = DB::instance(DB_NAME)->sanitize($_POST);
 
-    # Hash submitted password so we can compare it against one in the db
-    $_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+        # Hash submitted password so we can compare it against one in the db
+        $_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
 
-    # Search the db for this email and password
-    # Retrieve the token if it's available
-    $q = "SELECT token 
-        FROM users 
-        WHERE email = '".$_POST['email']."' 
-        AND password = '".$_POST['password']."'";
+        # Search the db for this email and password
+        # Retrieve the token if it's available
+         $q = "SELECT token 
+            FROM users 
+            WHERE email = '".$_POST['email']."' 
+            AND password = '".$_POST['password']."'";
 
-    $token = DB::instance(DB_NAME)->select_field($q);   
+        $token = DB::instance(DB_NAME)->select_field($q);   
 
-    # If we didn't get a token back, it means login failed
-    if(!$token) {
+        # If we didn't get a token back, it means login failed
+        if(!$token) {
 
         # Send them back to the login page
         Router::redirect("/users/login/error");
 
-    # But if we did, login succeeded! 
-    } else {
+        # But if we did, login succeeded! 
+        } else {
 
         /* 
         Store this token in a cookie using setcookie()
@@ -136,22 +138,23 @@ class users_controller extends base_controller {
     
     
     public function logout() {
-    #Generate and save a new token for next login
-    $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+        #Generate and save a new token for next login
+        $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
 
-    # Create the data array we'll use with the update method
-    # In this case, we're only updating one field, so our array only has one entry
-    $data = Array("token" => $new_token);
+        # Create the data array we'll use with the update method
+        # In this case, we're only updating one field, so our array only has one entry
+        $data = Array("token" => $new_token);
 
-    #Do the update
-    DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+        #Do the update
+        DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
 
-    #Delete their token cookie by setting it to a date in the past - effectively logging them out
-    setcookie("token", "", strtotime('-1 year'), '/');
+        #Delete their token cookie by setting it to a date in the past - effectively logging them out
+        setcookie("token", "", strtotime('-1 year'), '/');
 
-    #Send them back to the main index.
-    Router::redirect("/");
+        #Send them back to the main index.
+        Router::redirect("/");
     }
+
 
     public function profile($user_name = NULL) {
     
@@ -170,7 +173,7 @@ class users_controller extends base_controller {
         $data = DB::instance(DB_NAME)->select_row($q);
 
         #Build query to get user's posts
-       $q= 'SELECT 
+        $q= 'SELECT 
             posts.content,
             posts.created
         FROM posts
@@ -193,9 +196,10 @@ class users_controller extends base_controller {
         echo $this->template;
     }
 
-  public function upload() {
 
-    # Setup view
+    public function upload() {
+
+        # Setup view
         $this->template->content = View::instance('v_users_upload');
         $this->template->title   = "Profile";
 
@@ -206,23 +210,23 @@ class users_controller extends base_controller {
 
    public function p_upload() {
 
-    #Save image as a string and update row in the database
-    $image = Upload::upload($_FILES, "/uploads/profile/", array("jpg", "JPG", "jpeg", "JPEG","gif", "GIF","png", "PNG"), $this->user->user_id);
+        #Save image as a string and update row in the database
+        $image = Upload::upload($_FILES, "/uploads/profile/", array("jpg", "JPG", "jpeg", "JPEG","gif", "GIF","png", "PNG"), $this->user->user_id);
    
-    $imageFileName = dirname(__FILE__).'/../uploads/profile/'.$image;
-    $imageObj = new Image($imageFileName);
-    
-    #If image is large(2MB) resize will rotate the image
-    $imageObj->resize(150,150, "crop");
-    $imageObj->save_image($imageFileName); 
+        $imageFileName = dirname(__FILE__).'/../uploads/profile/'.$image;
+        $imageObj = new Image($imageFileName);
+
+        #If image is large(2MB) resize will rotate the image
+        $imageObj->resize(150,150, "crop");
+        $imageObj->save_image($imageFileName); 
     
 
-    $data=array("image"=>$image);
-    $dbInstance = DB::instance(DB_NAME);
-    $rows = $dbInstance->update("users", $data, "WHERE user_id = ".$this->user->user_id);
+        $data=array("image"=>$image);
+        $dbInstance = DB::instance(DB_NAME);
+        $rows = $dbInstance->update("users", $data, "WHERE user_id = ".$this->user->user_id);
 
-    #Send them back to the main index.
-    Router::redirect("/users/profile");
+        #Send them back to the main index.
+        Router::redirect("/users/profile");
     
    } 
 
